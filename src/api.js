1e9f3cd0a36c3ba883259844b244d7993935f073
SILATAKU2 <katerinataku@gmail.com>
2023-12-17 17:14:19 +0200

    Current weather API integration, forecast js start1.1038

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'becce78146msh85a938bc39e118bp1a8784jsna7cfa9bcc27c',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "fbb4e583d81aae6d5c4b46745deda757";
---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,25 @@
+import { Accordion, AccordionItemHeading, AccordionItem, AccordionItemPanel, AccordionItemButton } from "react-accessible-accordion";
+
+const Forecast = ({ data }) => {
+    return (
+        <>
+            <label className="title">Daily</label>
+            <Accordion allowZeroExpanded>
+                {data.list.splice(0, 7).map((item, idx) => (
+                    <AccordionItem key={idx}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                <div className="daily-item">
+                                    <img alt="weather" className="icon-small" src={`icons/${item}`} />
+                                </div>
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel></AccordionItemPanel>
+                    </AccordionItem>
+                ))}
+        </Accordion >
+        </>
+    );
+};
+
+export default Forecast;
